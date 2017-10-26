21d8f22ebb1a2015a3c740e0057f6e7c29b8bdf3
derP@765pro <Giratina493@mew151.net>
2017-10-26 16:14:39 -0700

    Removed devtools support for production

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import Root from './containers/Root';
 import registerServiceWorker from './registerServiceWorker';
 
+require('disable-react-devtools');
+
 ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
